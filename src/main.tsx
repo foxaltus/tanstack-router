ef0d1c2c4855d488e15972c44f7b286f86ec3cef
Guillaume Renard <guillaume.renard@altusgroup.com>
2024-11-07 11:11:19 +0100

    error page (not working)

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,20 @@ import "./index.css";
 // Set up a Router instance
 const router = createRouter({
   routeTree,
-  defaultPreload: "intent",
+  defaultNotFoundComponent: () => {
+    // const error = useRouteError();
+    // console.error(error);
+
+    return (
+      <div id="error-page">
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        {/* <p>
+          <i>{error.statusText || error.message}</i>
+        </p> */}
+      </div>
+    );
+  },
 });
 
 // Register things for typesafety
