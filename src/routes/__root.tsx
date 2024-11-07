8de281455c43709aaf8060d142cebc2aa96bed86
Guillaume Renard <guillaume.renard@altusgroup.com>
2024-11-07 11:03:26 +0100

    update root route

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,44 @@
 import * as React from "react";
-import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
   component: RootComponent,
 });
 
-function RootComponent() {
+export default function RootComponent() {
   return (
     <>
-      <Outlet />
+      <div id="sidebar">
+        <h1>TanStack Router Contacts</h1>
+        <div>
+          <form id="search-form" role="search">
+            <input
+              id="q"
+              aria-label="Search contacts"
+              placeholder="Search"
+              type="search"
+              name="q"
+            />
+            <div id="search-spinner" aria-hidden hidden={true} />
+            <div className="sr-only" aria-live="polite"></div>
+          </form>
+          <form method="post">
+            <button type="submit">New</button>
+          </form>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a href={`/contacts/1`}>Your Name</a>
+            </li>
+            <li>
+              <a href={`/contacts/2`}>Your Friend</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div id="detail"></div>
       <TanStackRouterDevtools position="bottom-right" />
     </>
   );
